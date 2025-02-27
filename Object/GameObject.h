8195bd226fcdb844b766e3a38f557c6c52c68363
minhyeok <minhyeogjeon945@gmail.com>
2025-02-27 13:35:53 +0900

    member variable hp
    getter setter

--- Object/GameObject.h
+++ Object/GameObject.h
@@ -7,9 +7,13 @@ public:
 	virtual ~GameObject();
 
 	unsigned long long GetId() const { return m_objectId; }
+	unsigned int GetHp() const { return m_hp; }
+
 	void SetId(unsigned long long id) { m_objectId = id; }
+	void SetHp(unsigned int hp) { m_hp = hp; }
 
 private:
 	unsigned long long m_objectId;
+	unsigned int m_hp = 0;
 };
 
