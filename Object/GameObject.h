3df7107acfc75967f835cf7774a862f4ca796155
minhyeok <minhyeogjeon945@gmail.com>
2025-02-27 13:34:24 +0900

    member variable objectId
    make getter setter

--- Object/GameObject.h
+++ Object/GameObject.h
@@ -5,5 +5,11 @@ class GameObject
 public:
 	GameObject() = default;
 	virtual ~GameObject();
+
+	unsigned long long GetId() const { return m_objectId; }
+	void SetId(unsigned long long id) { m_objectId = id; }
+
+private:
+	unsigned long long m_objectId;
 };
 
