a1d82a104a4db002a57a8472f83e220da1cb8334
minhyeok <minhyeogjeon945@gmail.com>
2025-02-28 11:51:51 +0900

    static HandlePacket(Session*, BYTE*, int)

--- ServerPacketHandler.h
+++ ServerPacketHandler.h
@@ -5,5 +5,10 @@ extern PacketHandlerFunc GPacketHandler[UINT16_MAX];
 
 class ServerPacketHandler
 {
+	static bool HandlePacket(Session* session, BYTE* buffer, int len)
+	{
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+		return GPacketHandler[header->id](session, buffer, len);
+	}
 };
 
