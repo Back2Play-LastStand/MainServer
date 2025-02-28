3525a4ef6f3512ab0100aaeb0ae6bcd2fe520b88
minhyeok <minhyeogjeon945@gmail.com>
2025-02-28 11:54:51 +0900

    make static Init()

--- ServerPacketHandler.h
+++ ServerPacketHandler.h
@@ -7,6 +7,13 @@ bool Handle_INVALID(Session* session, BYTE* buffer, int len);
 
 class ServerPacketHandler
 {
+public:
+	static void Init()
+	{
+		for (int i = 0; i < UINT16_MAX;i++)
+			GPacketHandler[i] = Handle_INVALID;
+	}
+
 	static bool HandlePacket(Session* session, BYTE* buffer, int len)
 	{
 		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
