81675ed9c350716fccfbf87d62e50147d70b3f57
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-12 15:14:19 +0900

    make CreateObject(Args&&...)

--- Manager/ObjectManager.h
+++ Manager/ObjectManager.h
@@ -4,5 +4,16 @@ class ObjectManager
 {
 public:
 	ObjectManager();
+	template<typename T, typename... Args>
+	shared_ptr<T> CreateObject(Args&&... args)
+	{
+		auto object = MakeShared<T>(m_idGenerator.fetch_add(1),forward<Args>(args)...);
+		m_objects.insert({m_idGenerator, object});
+		return object;
+	}
+
+private:
+	atomic<unsigned long long> m_idGenerator;
+	concurrent_unordered_map<unsigned long long, shared_ptr<class GameObject>> m_objects;
 };
 
