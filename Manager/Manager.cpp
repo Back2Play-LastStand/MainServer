3bda158a129bb7146a712fedd95f59121ec61a86
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-13 14:02:03 +0900

    build error solved
    make_pair rollback & include in manager

--- Manager/Manager.cpp
+++ Manager/Manager.cpp
@@ -1,9 +1,12 @@
 #include "pch.h"
 #include "Manager.h"
+#include "ObjectManager.h"
+#include "RoomManager.h"
 
 Manager::Manager()
 {
 	m_object = MakeShared<ObjectManager>();
+	m_room = MakeShared<RoomManager>();
 }
 
 Manager::~Manager()
