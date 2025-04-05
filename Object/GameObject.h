995c98ed910bfc7afddd342d0f7e1a741f47aac3
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-05 15:18:38 +0900

    update GameObject
    make getter setter

--- Object/GameObject.h
+++ Object/GameObject.h
@@ -9,10 +9,12 @@ public:
 	virtual ~GameObject();
 
 	unsigned long long GetId() const { return m_objectId; }
+	Protocol::ObjectType GetType() const { return m_objectType; }
 	unsigned int GetHp() const { return m_hp; }
 	unsigned int GetPower() const { return m_power; }
 
 	void SetId(unsigned long long id) { m_objectId = id; }
+	void SetType(Protocol::ObjectType type) { m_objectType = type; }
 	void SetHp(unsigned int hp) { m_hp = hp; }
 	void SetPower(unsigned int power) { m_power = power; }
 
