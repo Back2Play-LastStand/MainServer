9591d22abbab400a12f1ae8bd681cfaabec25f62
hyuli0520 <minhyeogjeon945@gmail.com>
2025-05-06 20:54:59 +0900

    delete Monster State
    delete monster FSM in server

--- Object/Monster.h
+++ Object/Monster.h
@@ -22,16 +22,9 @@ public:
 	void SetObjectInfo(Protocol::ObjectInfo info);
 	Protocol::ObjectInfo& GetObjectInfo();
 
-	float GetDistance(const Protocol::PositionInfo& myPos, const Protocol::PositionInfo& targetPos);
-	void UpdateState(float dist);
-
 	virtual void BeginPlay() override;
 	virtual void Tick() override;
 	virtual void TakeDamage(int amount) override;
-
-	virtual void Idle();
-	virtual void Chase();
-	virtual void Attack();
 
 private:
 	shared_ptr<Room> m_room;
